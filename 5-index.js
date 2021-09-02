f1cf5d1049e68eaf2e6bd36e8e087471e282d9f6
Fabio Leardini <leardinifabio90@gmail.com>
2021-09-02 00:27:04 -0300

    0026-02092021

--- 5-index.js
+++ 5-index.js
@@ -1,22 +1,37 @@
 class Cliente { //criando molde com propriedades do cliente.
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    _saldo;
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this._saldo += valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente(); //instanciando objeto.
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 13000177700;
-cliente1.agencia = 1370;
-cliente1.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1370;
+contaCorrenteRicardo._saldo = 0;
+
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 13000066699;
-cliente2.agencia = 1370;
-cliente2.saldo = 3000;
 
-console.log(cliente1);
-console.log(cliente2);
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.agencia = 1370;
+contaCorrenteAlice._saldo = 3000;
