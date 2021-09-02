bad096c30d9c9db6c070bfdfcf74d408ba20aae5
Fabio Leardini <leardinifabio90@gmail.com>
2021-09-02 00:38:25 -0300

    0038-02092021

--- 5-index.js
+++ 5-index.js
@@ -10,12 +10,14 @@ class ContaCorrente {
     sacar(valor) {
         if (this._saldo >= valor) {
             this._saldo -= valor;
+            return valor;
         }
     }
 
     depositar(valor) {
         if (valor > 0) {
             this._saldo += valor;
+            return valor;
         }
     }
 }
